c4a63cd0bf1eb794776343d9eda52f5eb37f3491
xfarkasp <xfarkasp@stuba.sk>
2023-02-25 11:19:43 +0100

    AA and AVL testing changes

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,42 @@ public class Main {
     public static void main(String[] args) {
         AVLTree ygdrasil = new AVLTree();
 
-        ygdrasil.insert(9, "pain");
-        ygdrasil.insert(5, "dd");
-        ygdrasil.insert(10, "aa");
-        ygdrasil.insert(0, "paissn");
-        ygdrasil.insert(6, "hhh");
-        ygdrasil.insert(11, "jjjj");
-        ygdrasil.insert(1, "jjjj");
-        ygdrasil.insert(2, "jjjj");
+        /*ygdrasil.insert("pain");
+        ygdrasil.insert("dd");
+        ygdrasil.insert("aa");
+        ygdrasil.insert("cc");
+        ygdrasil.insert("pais");
+        ygdrasil.insert("pain");
+        ygdrasil.insert("p");
+        ygdrasil.insert("abcd");
 
-        ygdrasil.delete(10);
-        System.out.println("oh please go help me");
+        ygdrasil.insert(1);
+        ygdrasil.insert(5);
+        ygdrasil.insert(89);
+        ygdrasil.insert(56);
 
-        ygdrasil.search(5);
-        ygdrasil.search(9);
-        ygdrasil.search(10);
-        ygdrasil.search(1000);
-        ygdrasil.search(12);
-        ygdrasil.search(11);
-        ygdrasil.search(2);
-        ygdrasil.search(6);
-        ygdrasil.search(0);
+
+        ygdrasil.delete("p");
+        ygdrasil.delete("abcd");
+        ygdrasil.delete("penis");
+
+
+        ygdrasil.search("pain");
+        ygdrasil.search("dd");
+        ygdrasil.search("aa");
+        ygdrasil.search("pais");
+        ygdrasil.search("p");
+        ygdrasil.search("abcd");*/
+
+        AATree nimloth = new AATree();
+
+        nimloth.insert("penis");
+        nimloth.insert("kokot");
+        nimloth.insert("pica");
+        nimloth.insert("u");
+        nimloth.insert("holica");
+
+        nimloth.delete("pica");
+        nimloth.delete("penis");
     }
 }
